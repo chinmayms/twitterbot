bf3963caaa4d4b5427bd354353b44b84bba505d0
chinmayms <chinmaysumant@gmail.com>
2017-04-10 17:20:28 -0400

    Random Forest and Gaussian NB models complete

---
+++ naive_bayes_twitter_classififer.py
@@ -0,0 +1,65 @@
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import numpy as np
+
+# create data frame containing your data, each column can be accessed # by df['column   name']
+df_bot = pd.read_csv('bots_data.csv',encoding='latin-1')
+df_non_bot = pd.read_csv('nonbots_data.csv',encoding='latin-1')
+df = pd.concat([df_bot,df_non_bot])
+
+def verified(df):
+    if df['verified'] == 'TRUE':
+        return 1
+    else:
+        return 0
+
+def default_profile(df):
+    if df['default_profile'] == 'TRUE':
+        return 1
+    else:
+        return 0
+
+def default_profile_image(df):
+    if df['default_profile_image'] == 'TRUE':
+        return 1
+    else:
+        return 0
+
+
+df['verified'] = df.apply(verified,axis=1)
+df['default_profile_image'] = df.apply(default_profile_image,axis=1)
+df['default_profile'] = df.apply(default_profile,axis=1)
+
+df = df[['followers_count','friends_count','listedcount','verified','statuses_count','default_profile','default_profile_image','bot']]
+
+# print(df.head())
+
+
+
+# add columns to your data frame
+df['is_train'] = np.random.uniform(0, 1, len(df)) <= 0.75
+
+
+# define training and test sets
+train = df[df['is_train'] == True]
+test = df[df['is_train'] == False]
+
+trainTargets = np.array(train['bot']).astype(int)
+testTargets = np.array(test['bot']).astype(int)
+
+
+
+# columns you want to model
+features = df.columns[0:7]
+
+# call Gaussian Naive Bayesian class with default parameters
+gnb = GaussianNB()
+
+
+# train model
+
+
+gnb.fit(train[features], trainTargets).predict(train[features])
+
+print(gnb.score(test[features],testTargets))
