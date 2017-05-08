5137d2579e967ea71a3377515e715a346f92a082
chinmayms <chinmaysumant@gmail.com>
2017-05-08 19:00:16 -0400

    random_forest code complete

--- random_forest_twitter_classifier.py
+++ random_forest_twitter_classifier.py
@@ -1,26 +1,18 @@
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-import pandas as pd
-from nltk.tokenize import WordPunctTokenizer,word_tokenize
-import numpy as np
-from sklearn.model_selection import cross_val_score, KFold, train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report,confusion_matrix
-from sklearn.feature_extraction.text import *
-from random import choice
-from string import ascii_uppercase
+#Python imports
 import timestring
 from datetime import datetime
+import pandas as pd
 
-#create data frame containing your data, each column can be accessed # by df['column   name']
-df_bot = pd.read_csv('bots_data.csv',encoding='latin-1')
-df_non_bot = pd.read_csv('nonbots_data.csv',encoding='latin-1')
-# df = pd.concat([df_bot,df_non_bot])
+#Sklearn Imports
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix
 
+
+#Load training data into a dataframe
 df = pd.read_csv('training_data_2_csv_UTF.csv')
 
-
+#Methods to apply on dataframe for data cleaning
 def verified(df):
     if df['verified'] == 'TRUE':
         return 1
@@ -102,15 +94,7 @@ def days(df):
 
     return 365
 
-#dt_1 = parser.parse(df['created_at'][0])
-
-print(type(df['created_at'][0]))
-dt_2 = datetime.strptime(str(timestring.Date(df['created_at'][0])),"%Y-%m-%d %H:%M:%S")
-dt_1 = datetime.strptime(str(timestring.Date(df['created_at'][1])),"%Y-%m-%d %H:%M:%S")
-
-print(abs(datetime.now()-dt_2).days)
-
-
+#Apply functions to clean columns of dataframe
 df['verified'] = df.apply(verified,axis=1)
 df['default_profile_image'] = df.apply(default_profile_image,axis=1)
 df['default_profile'] = df.apply(default_profile,axis=1)
@@ -121,57 +105,63 @@ df['location'] = df.apply(length_location,axis=1)
 df['len_desc'] = df.apply(length_desc,axis=1)
 df['ep'] = df.apply(extended_profile,axis=1)
 df['date'] = df.apply(days,axis=1)
+
+#Feature selection, drop unwanted columns from dataframe
 df = df[['followers_count','friends_count','listedcount','verified','statuses_count','default_profile','default_profile_image','name','name_only','desc','location','len_desc','favourites_count','ep','date','bot']]
 
-print(df.shape)
-
-df.to_csv('intermediate.csv',index=False)
-
+#USe train test split to split data into training and testing sets
 X_train,X_test,y_train,y_test = train_test_split(df.drop('bot',axis=1),df['bot'],test_size=0.1)
 
 
-# call Gaussian Naive Bayesian class with default parameters
-
+#Declare Random Forest classifier Object
 rf = RandomForestClassifier(n_estimators=100,criterion='entropy')
 
-# train model
+# train model by calling fit method
 rf.fit(X_train,y_train)
 
+#Store predictions on test set (Cross Validation)
 preds = rf.predict(X_test)
 
+
+#Take a look at the classification report and confusion matrix to evaluate model accuracy
 print(classification_report(y_test,preds))
 print(confusion_matrix(y_test,preds))
 
-# df_test = pd.read_csv('test_data_4_students.csv')
-#
-# print(df_test.shape)
-#
-# df_test['verified'] = df_test.apply(verified,axis=1)
-# df_test['default_profile_image'] = df_test.apply(default_profile_image,axis=1)
-# df_test['default_profile'] = df_test.apply(default_profile,axis=1)
-# df_test['name'] = df_test.apply(name_bot,axis=1)
-# df_test['name_only'] = df_test.apply(name_only_bot,axis=1)
-# df_test['desc'] = df_test.apply(desc_bot,axis=1)
-# df_test['location'] = df_test.apply(length_location,axis=1)
-# df_test['len_desc'] = df_test.apply(length_desc,axis=1)
-# df_test['ep'] = df_test.apply(extended_profile,axis=1)
-# df_test['listed_count'] = df_test.apply(listed_count,axis=1)
-# df_test['friends_count'] = df_test.apply(friends_count,axis=1)
-# df_test['followers_count'] = df_test.apply(followers,axis=1)
-# df_test['date'] = df_test.apply(days,axis=1)
-# df_test = df_test[['followers_count','friends_count','listed_count','verified','statuses_count','default_profile','default_profile_image','name','name_only','desc','location','len_desc','favorites_count','ep','date']]
-#
-# df_test.to_csv('test_clean.csv')
 
+#Load Test data into dataframe
+df_test = pd.read_csv('test_data_4_students.csv')
+
+#Apply functions to clean test dataset
+df_test['verified'] = df_test.apply(verified,axis=1)
+df_test['default_profile_image'] = df_test.apply(default_profile_image,axis=1)
+df_test['default_profile'] = df_test.apply(default_profile,axis=1)
+df_test['name'] = df_test.apply(name_bot,axis=1)
+df_test['name_only'] = df_test.apply(name_only_bot,axis=1)
+df_test['desc'] = df_test.apply(desc_bot,axis=1)
+df_test['location'] = df_test.apply(length_location,axis=1)
+df_test['len_desc'] = df_test.apply(length_desc,axis=1)
+df_test['ep'] = df_test.apply(extended_profile,axis=1)
+df_test['listed_count'] = df_test.apply(listed_count,axis=1)
+df_test['friends_count'] = df_test.apply(friends_count,axis=1)
+df_test['followers_count'] = df_test.apply(followers,axis=1)
+df_test['date'] = df_test.apply(days,axis=1)
+
+#Remove unwanted columns (Feature Selection)
+df_test = df_test[['followers_count','friends_count','listed_count','verified','statuses_count','default_profile','default_profile_image','name','name_only','desc','location','len_desc','favorites_count','ep','date']]
+
+#Store cleaned test data to disk for future use
+df_test.to_csv('test_clean.csv')
+
+#Load cleaned test dataset
 df_test = pd.read_csv('test_clean.csv')
 
-
+#Run model onto test data and get predictions
 kaggle_preds = rf.predict(df_test)
 
-print(kaggle_preds)
-
+#Add predictions to test dataframe
 df_test['bot'] = kaggle_preds
 
+#Save dataframe with predictions to disk
 df_test.to_csv('answers.csv')
 
 
